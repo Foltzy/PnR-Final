23ea06c8225f77dc521d5f8db771a85e42d569d1
foltzb <ben.p.foltz@gmail.com>
2016-10-07 08:56:29 -0400

    Started student dance

--- student.py
+++ student.py
@@ -49,7 +49,8 @@ class GoPiggy(pigo.Pigo):
         ##### WRITE YOUR FIRST PROJECT HERE
         x = 100
         a = 0
-        while self.isClear() and a <= 100:
+        while a <= 100:
+            self.isClear()
             self.encR(5)
             a += 25
 
