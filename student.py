da04d209d8a47f69e743b0c94591354939321330
foltzb <ben.p.foltz@gmail.com>
2016-11-18 09:33:03 -0500

    New turn method -- int added

--- student.py
+++ student.py
@@ -159,8 +159,8 @@ class GoPiggy(pigo.Pigo):
         self.setSpeed(self.LEFT_SPEED, self.RIGHT_SPEED)
 
     def setSpeed(self, left, right):
-        set_left_speed(left)
-        set_right_speed(right)
+        set_left_speed(int(left))
+        set_right_speed(int(right))
         time.sleep(.05)
 
 
